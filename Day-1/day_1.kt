4cd66c484a3f0963d346ff8b77d661e6bb02b48e
ADITYA NRT <rahul.knrt@gmail.com>
2024-07-13 17:20:45 +0530

    Initial commit

---
+++ Day-1/day_1.kt
@@ -0,0 +1,34 @@
+ fun main(args: Array<String>) {
+    
+    var myint=23
+    var mylong=35L
+
+    println("My interger ${myint}")
+    println("My Long ${mylong}")
+
+    var b1:Byte=Byte.MIN_VALUE
+    var b2:Byte=Byte.MAX_VALUE
+
+    println("smallest b1 ${b1}")
+    println("Largest b2 ${b2}")
+
+
+    var s1:Short=Short.MIN_VALUE
+    var s2:Short=Short.MAX_VALUE
+
+    println("smallest s1 ${s1}")
+    println("Largest s2 ${s2}")
+
+    var i1:Int=Int.MIN_VALUE
+    var i2:Int=Int.MAX_VALUE
+
+    println("smallest i1 ${i1}")
+    println("Largest i2 ${i2}")
+
+    var l1:Long=Long.MIN_VALUE
+    var l2:Long=Long.MAX_VALUE
+
+    println("smallest l1 ${l1}")
+    println("Largest l2 ${l2}")
+
+}
---
+++ Day-1/variable.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    
+    var roll=101
+    var name="RAHUL KUMAR"
+    println("My roll number $roll")
+    println(name)
+
+    // Immutable using val keyword
+    
+}
