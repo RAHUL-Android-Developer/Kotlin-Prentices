37efd99864a41f561a17600716e7344798723d3f
ADITYA NRT <rahul.knrt@gmail.com>
2024-07-13 17:37:08 +0530

    Variable

--- Day-1/variable.kt
+++ Day-1/variable.kt
@@ -5,6 +5,21 @@ fun main(args: Array<String>) {
     println("My roll number $roll")
     println(name)
 
-    // Immutable using val keyword
+// Immutable using val keyword
+// Mutable using var keyword
+
+// 1.Immutable using val keyword
+
+val userName="Raja"
+println("$userName")
+// userName ="Rahul" //Compile time error val cannot be reassigned
+
+var dob="21/02/2002";
+val dobVal =dob;
+
+    println("$dobVal")
+
+    var birthDate="04 may 2002";
+    println("$birthDate")
     
 }
